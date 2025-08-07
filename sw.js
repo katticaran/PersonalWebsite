3924b2a8856e0caf36aafe69c3fbba1bc282560a
Joel Katticaran <jkatticaran@Joels-MacBook-Air.local>
2025-08-07 14:19:56 -0700

    website improvements

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'joel-katticaran-v1';
+const urlsToCache = [
+  '/',
+  '/css/master.css',
+  '/css/animate.css',
+  '/js/wow.min.js',
+  '/images/portrait.jpg',
+  '/images/background.jpg',
+  '/images/icon.svg',
+  '/docs/JoelKatticaran_Resume_2025.pdf'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
